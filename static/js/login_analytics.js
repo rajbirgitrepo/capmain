b9bf9a5e9afc27e33f73baeff5d4e9668a1d44b1
kj1gen <kartik@1gen.io>
2021-08-26 17:52:50 +0530

    latest

--- static/js/login_analytics.js
+++ static/js/login_analytics.js
@@ -68,7 +68,11 @@ var settings = {
         pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
     },
     plotOptions: {series: {point: {
-             
+        events: {
+            click: function () {
+              alert(this.x);
+            },
+          },
             }},
         column: {
             stacking: 'normal',
@@ -91,4 +95,105 @@ var settings = {
     
      ]
   });});
-  
+
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "/daily_logins",
+    method: "GET",
+  };
+  $.ajax(settings).done(function (response) {
+    var dataa = JSON.parse(response);
+    console.log(dataa);
+    $(function () {
+      $("#container2").highcharts({
+        chart: {
+          zoomType: "xy",
+              type: "column"
+        },
+        title: {
+          text: "Successful Login History",
+        },
+        colors: ['#4F1FAF','#462CEE','#8AE02B','#01A451','#33FFD1'],
+        xAxis: [
+          {
+            categories: dataa.date,
+            labels: {
+              rotation : 90,
+            }
+          },
+          
+        ],
+        yAxis: [
+          {
+            //Primary yAxis
+            lineWidth: 1,
+            labels: {
+              format: "{value}",
+              style: {
+                color: "#000",
+              },
+            },
+            title: {
+              text: "Login Count",
+              style: {
+                color: "#000",
+              },
+            },
+          },
+          {
+            //Secondary yAxis
+            title: {
+              text: "",
+              style: {
+                color: "#4572A7",
+              },
+            },
+            labels: {
+              format: "{value}",
+              style: {
+                color: "#4572A7",
+              },
+            },
+            opposite: false,
+          },
+        ],
+        tooltip: {
+          shared: true,
+        },
+        plotOptions: { borderWidth: 2,
+          series: {point: {
+           
+        }},
+    column: {
+        stacking: 'normal',
+        dataLabels: {
+            enabled: false
+        }
+    } },
+        series: [
+          {
+            name: 'Clever',
+            data: dataa.clever_success_login
+        },
+        {
+          name: 'Schoology',
+          data: dataa.schoology_success_login
+        },{
+              name: 'Homeapp',
+              fontSize:'8px',
+              data: dataa.homeapp_success_login
+            
+          }, {
+              name: 'Webapp',
+              data: dataa.webapp_success_login
+          },
+{
+              name: 'Classroom',
+              data: dataa.classroom_success_login
+          },
+        ],
+      });
+    });
+  });
+
