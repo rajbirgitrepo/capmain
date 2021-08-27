ed4c29d86fb7e008e918a0309dd4f6c475c2a56b
sarthak1gen <78341927+sarthak1gen@users.noreply.github.com>
2021-08-27 14:38:04 +0530

    comiyt

--- static/js/schoolsummary2.js
+++ static/js/schoolsummary2.js
@@ -1084,8 +1084,8 @@ var settings = {
                 type: 'column'
               },
               colors: [
-                '#01A451',
                 '#8AE02B',
+                '#01A451',
                 '#a0afb0',
                 '#01A451',
                          ],
@@ -1126,12 +1126,12 @@ var settings = {
               series: [{
                 name: 'Total users with atleast 5 Playbacks',
                 data: dataa.total_user_atleast_5_playbacks,
-                stack: 0
+                stack: 1
               },
                       {
                 name: 'Total Useres',
                 data: dataa.total_users,
-                stack: 1
+                stack: 0
               }]
             });
             
